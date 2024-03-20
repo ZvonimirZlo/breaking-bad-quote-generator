04a54e1635b73aa76c449a24908c393dc69e3220
ZvonimirZlo <lovrez2@gmail.com>
2024-03-21 00:37:22 +0100

    fetched api

--- quote-generator/src/App.js
+++ quote-generator/src/App.js
@@ -1,10 +1,18 @@
-
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [quotes, setQuotes] = useState('');
+
+  const getQuote = () => {
+    fetch('https://api.breakingbadquotes.xyz/v1/quotes')
+    .then(res => res.json())
+    .then(data => console.log(data));
+  }
+
   return (
     <div className='App'>
-      
+      <button onClick={getQuote}>Get Quotes</button>
     </div>
   );
 }
