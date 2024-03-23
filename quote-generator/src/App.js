6d3c2e35a5d0929637113c76bb787f1f9b631d84
ZvonimirZlo <lovrez2@gmail.com>
2024-03-23 19:49:24 +0100

    added styles

--- quote-generator/src/App.js
+++ quote-generator/src/App.js
@@ -14,17 +14,16 @@ function App() {
   return (
     <>
       <div>
-        <div className='cont container-sm '>
+        <div className='cont container'>
           <div className='text'>
             <h3 className='animate__animated animate__fadeIn fw-bold'>{quotes.quote}</h3>
             <h4 className='animate__animated animate__fadeIn text-end fst-italic'>{quotes.author}</h4>
-            <button className='animate__animated animate__flip btn btn-outline-dark btn-lg fw-bold' onClick={getQuote}>Get Quote</button>
+            <button className='animate__animated animate__flip fw-bold' onClick={getQuote}>Get Quote</button>
           </div>
           
         </div>
         
       </div>
-
       <footer className="footer">© Made by ZvonimirZlo</footer>
     </>
   );
